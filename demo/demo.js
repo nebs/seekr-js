f2b56356e3485eeca112a333e0a472db8469557b
Nebojsa Petrovic <nebs@causes.com>
2014-01-11 13:13:29 -0800

    Add interactive grid
    This commit adds the ability to add and remove walls by clicking on
    cells in the grid. This dynamically updates the path finding and
    redraws.
    
    To make this happen I've added the ability for the artist to handle
    click events.
    
    Via a callback, the demo.js can now redraw and find a new path each time
    a cell is clicked.

--- demo/demo.js
+++ demo/demo.js
@@ -1,21 +1,13 @@
-window.onload = function() {
-  var grid     = new Grid2D([[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-                             [0, 1, 0, 0, 1, 0, 0, 0, 1, 0],
-                             [0, 1, 0, 0, 1, 0, 0, 1, 0, 0],
-                             [0, 1, 0, 0, 0, 0, 1, 0, 3, 0],
-                             [0, 1, 1, 1, 1, 1, 0, 0, 0, 0],
-                             [0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
-                             [0, 1, 2, 1, 0, 1, 0, 0, 0, 0],
-                             [0, 1, 0, 1, 0, 1, 0, 0, 0, 0],
-                             [0, 1, 0, 1, 0, 1, 0, 0, 0, 0],
-                             [0, 0, 0, 0, 0, 1, 0, 0, 0, 0]]),
-      canvas   = document.getElementById('main-canvas'),
-      artist   = new Grid2DArtist(grid, canvas),
-      delegate = new SeekrGrid2DDelegate(grid),
-      seekr    = new Seekr(delegate),
-      path     = seekr.seek();
+function refresh(grid, seekr, artist) {
+  var path = seekr.seek();
 
-  // Add the path to the grid
+  grid.loop(function(x, y, cell) {
+    if (cell == 4) {
+      grid.cellAt(x, y, 0);
+    }
+    return true;
+  });
+
   grid.loop(function(x, y, cell) {
     for (var i in path) {
       if (path[i].x == x && path[i].y == y && cell != 3 && cell != 2) {
@@ -23,9 +15,38 @@ window.onload = function() {
         break;
       }
     }
-
     return true;
   });
 
   artist.draw();
+}
+
+window.onload = function() {
+  var grid     = new Grid2D([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                             [0, 0, 0, 0, 0, 0, 0, 0, 3, 0],
+                             [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                             [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                             [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                             [0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+                             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]),
+      canvas   = document.getElementById('main-canvas'),
+      artist   = new Grid2DArtist(grid, canvas),
+      delegate = new SeekrGrid2DDelegate(grid),
+      seekr    = new Seekr(delegate);
+
+  refresh(grid, seekr, artist);
+
+  artist.onClick = function(x, y) {
+    var cell = grid.cellAt(x, y);
+    if (cell == 1) {
+      grid.cellAt(x, y, 0);
+    } else if (cell == 0 || cell == 4) {
+      grid.cellAt(x, y, 1);
+    }
+
+    refresh(grid, seekr, artist);
+  }
 }
--- grid2d/grid-2d-artist.js
+++ grid2d/grid-2d-artist.js
@@ -7,7 +7,18 @@ function Grid2DArtist(grid, canvas) {
   this.cellHeight = this.height / this.grid.rowCount(),
   this.cellWidth  = this.width  / this.grid.colCount();
   this.colors     = ['white', 'black', 'blue', 'red', 'yellow'];
+  this.canvas.addEventListener('click', this.canvasClicked.bind(this), false);
 }
+
+Grid2DArtist.prototype.canvasClicked = function(evt) {
+  var xPx = evt.pageX - this.canvas.offsetLeft,
+      yPx = evt.pageY - this.canvas.offsetTop,
+      x   = Math.floor(xPx / this.cellWidth),
+      y   = Math.floor(yPx / this.cellHeight);
+  if (this.onClick) {
+    this.onClick(x, y);
+  }
+};
 
 Grid2DArtist.prototype.draw = function() {
   this.grid.loop(function(x, y, cell, yPx) {
